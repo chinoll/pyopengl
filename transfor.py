f5d5016e814e995b17346fc42efb122f3e654282
chinoll <chinoll@protonmail.com>
2021-03-24 20:28:34 +0800

    二维旋转

--- transfor.py
+++ transfor.py
@@ -2,9 +2,10 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 from antialiasing import *
-
+from math import *
 def transforPolygan(x,y,tx,ty):
     '''
+        将图元平移
         x:坐标x的集合
         y:坐标y的集合
         tx:移动x的增量
@@ -13,8 +14,24 @@ def transforPolygan(x,y,tx,ty):
     points = []
     for i in range(len(x)):
         points.append((x[i] + tx,y[i] + ty))
-    #print("move",points)
-    glBegin(GL_POINTS)
+    for point in points:
+        antialiasing(point[0],point[1])
+    return points
+
+def rotatePolygan(x,y,tx,ty,theta):
+    '''
+        将图元绕轴旋转
+        x:坐标x的集合
+        y:坐标y的集合
+        tx:轴的x坐标
+        ty:轴的y坐标
+        theta:旋转角度
+    '''
+    points = []
+    for i in range(len(x)):
+        nx = tx + (x[i] - tx)*cos(theta) - (y[i] - ty)*sin(theta)
+        ny = ty + (x[i] - tx)*sin(theta) + (y[i] - ty)*cos(theta)
+        points.append((nx,ny))
     for point in points:
         antialiasing(point[0],point[1])
     return points
